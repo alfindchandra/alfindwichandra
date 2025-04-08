ec17aa649e8e236ef1b9ff44319e8e0c4e40e84b
alfindchandra <alfindwichandra55@gmail.com>
2025-04-08 22:23:01 +0700

    hapus

--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -70,7 +70,7 @@ const ProjectsPage: React.FC = () => {
                 </h3>
                 <p className="text-gray-300 mb-6">{project.description}</p>
                 <Link
-                  href={project.link}
+                  href=""
                   className="text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-sm px-7 py-2.5 text-center me-2 mb-2"
                 >
                   Lihat

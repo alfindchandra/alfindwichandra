fac2723bf5e41a69f43e8580285ab446721d4feb
alfindchandra <alfindwichandra55@gmail.com>
2025-04-11 14:38:15 +0700

    baru

--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-import { Code, Globe, Github } from "lucide-react";
+import { Github } from "lucide-react";
 import Image from "next/image";
 
 export default function Projects() {
